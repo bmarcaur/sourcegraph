0b244c3a267e4379212dba894ed010460b7e3361
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-08-30 17:06:35 +0200

    WIP

--- client/shared/src/codeintel/api.ts
+++ client/shared/src/codeintel/api.ts
@@ -1,6 +1,14 @@
-import { HoverMerged, TextDocumentPositionParameters } from '@sourcegraph/client-api'
+import { HoverMerged, TextDocumentIdentifier, TextDocumentPositionParameters } from '@sourcegraph/client-api'
 import * as sourcegraph from './legacy-extensions/api'
 import * as clientType from '@sourcegraph/extension-api-types'
+import { languageSpecs } from './legacy-extensions/language-specs/languages'
+import { LanguageSpec } from './legacy-extensions/language-specs/spec'
+import { DocumentSelector, TextDocument } from 'sourcegraph'
+import { match } from '../api/client/types/textDocument'
+import { getModeFromPath } from '../languages'
+import { parseRepoURI } from '../util/url'
+import { createProviders, SourcegraphProviders } from './legacy-extensions/providers'
+import { RedactingLogger } from './legacy-extensions/logging'
 
 export type QueryGraphQLFn<T> = () => Promise<T>
 
@@ -31,6 +39,7 @@ class DefaultCodeIntelAPI implements CodeIntelAPI {
         throw new Error('Method not implemented.')
     }
     getDefinition(textParameters: TextDocumentPositionParameters): Promise<clientType.Location[]> {
+        textParameters.textDocument
         return Promise.resolve([])
     }
     getHover(textParameters: TextDocumentPositionParameters): Promise<HoverMerged> {
@@ -40,4 +49,37 @@ class DefaultCodeIntelAPI implements CodeIntelAPI {
     getDocumentHighlights(textParameters: TextDocumentPositionParameters): Promise<sourcegraph.DocumentHighlight[]> {
         return Promise.resolve([])
     }
+}
+
+function findLanguageMatchingDocument(textDocument: TextDocumentIdentifier): Language | undefined {
+    const document: Pick<TextDocument, 'uri' | 'languageId'> = {
+        uri: textDocument.uri,
+        languageId: getModeFromPath(parseRepoURI(textDocument.uri).filePath || ''),
+    }
+
+    for (const language of languages) {
+        if (match(language.selector, document)) {
+            return language
+        }
+    }
+    return undefined
+}
+interface Language {
+    spec: LanguageSpec
+    selector: DocumentSelector
+    providers: SourcegraphProviders
+}
+const hasImplementationsField = true
+const languages: Language[] = languageSpecs.map(spec => ({
+    spec,
+    selector: selectorForSpec(spec),
+    providers: createProviders(spec, hasImplementationsField, new RedactingLogger(console)),
+}))
+
+function selectorForSpec(languageSpec: LanguageSpec): DocumentSelector {
+    return [
+        { language: languageSpec.languageID },
+        ...(languageSpec.verbatimFilenames || []).flatMap(filename => [{ pattern: filename }]),
+        ...languageSpec.fileExts.flatMap(extension => [{ pattern: `*.${extension}` }]),
+    ]
 }
--- client/shared/src/codeintel/legacy-extensions/extension.ts
+++ client/shared/src/codeintel/legacy-extensions/extension.ts
@@ -20,39 +20,6 @@ const DUMMY_CTX = {
     },
 }
 
-const hasImplementationsField = once(() => new API().hasImplementationsField())
-
-/**
- * Create the panel for implementations.
- *
- * Makes sure to only create the panel once per session.
- */
-const createImplementationPanel = (
-    context: sourcegraph.ExtensionContext = DUMMY_CTX,
-    selector: sourcegraph.DocumentSelector,
-    providers: SourcegraphProviders
-): void => {
-    const implementationsPanelID = 'implementations_LANGID'
-    const implementationsPanel = sourcegraph.app.createPanelView(implementationsPanelID)
-
-    implementationsPanel.title = 'Implementations'
-    implementationsPanel.component = { locationProvider: implementationsPanelID }
-    implementationsPanel.priority = 160
-    implementationsPanel.selector = selector
-
-    const maxPanelResults = sourcegraph.configuration
-        .get<{ 'codeIntelligence.maxPanelResults'?: number }>()
-        .get('codeIntelligence.maxPanelResults')
-    if (maxPanelResults) {
-        implementationsPanel.component.maxLocationResults = maxPanelResults
-    }
-
-    context.subscriptions.add(implementationsPanel)
-    context.subscriptions.add(
-        sourcegraph.languages.registerLocationProvider(implementationsPanelID, selector, providers.implementations)
-    )
-}
-
 /**
  * Activate the extension by registering definition, reference, and hover providers
  * with LSIF and search-based providers.
@@ -71,51 +38,26 @@ export const activate = async (context: sourcegraph.ExtensionContext = DUMMY_CTX
         ...languageSpec.fileExts.flatMap(extension => [{ pattern: `*.${extension}` }]),
     ]
 
-    const hasImplementationsFieldConst = await hasImplementationsField()
+    const hasImplementationsFieldConst = true
 
     const providers = createProviders(languageSpec, hasImplementationsFieldConst, new RedactingLogger(console))
     context.subscriptions.add(sourcegraph.languages.registerDefinitionProvider(selector, providers.definition))
     context.subscriptions.add(sourcegraph.languages.registerHoverProvider(selector, providers.hover))
-
-    // Do not try to register this provider on pre-3.18 instances as
-    // it didn't exist.
-    if (sourcegraph.languages.registerDocumentHighlightProvider) {
-        context.subscriptions.add(
-            sourcegraph.languages.registerDocumentHighlightProvider(selector, providers.documentHighlights)
-        )
-    }
-
-    // Re-register the references provider whenever the value of the
-    // mixPreciseAndSearchBasedReferences setting changes.
-
-    let unsubscribeReferencesProvider: sourcegraph.Unsubscribable
-    const registerReferencesProvider = (): void => {
-        unsubscribeReferencesProvider?.unsubscribe()
-        unsubscribeReferencesProvider = sourcegraph.languages.registerReferenceProvider(selector, providers.references)
-        context.subscriptions.add(unsubscribeReferencesProvider)
-    }
-
     context.subscriptions.add(
-        from(sourcegraph.configuration)
-            .pipe(
-                startWith(false),
-                map(() => sourcegraph.configuration.get().get('codeIntel.mixPreciseAndSearchBasedReferences') ?? false),
-                distinctUntilChanged(),
-                map(registerReferencesProvider)
-            )
-            .subscribe()
+        sourcegraph.languages.registerDocumentHighlightProvider(selector, providers.documentHighlights)
     )
+    context.subscriptions.add(sourcegraph.languages.registerReferenceProvider(selector, providers.references))
 
-    if (hasImplementationsFieldConst) {
-        if (languageSpec.textDocumentImplemenationSupport) {
-            // Show the "Find implementations" button in the hover as specified in package.json (look for
-            // "findImplementations").
-            sourcegraph.internal.updateContext({
-                ['implementations_LANGID']: true,
-            })
+    // if (hasImplementationsFieldConst) {
+    //     if (languageSpec.textDocumentImplemenationSupport) {
+    //         // Show the "Find implementations" button in the hover as specified in package.json (look for
+    //         // "findImplementations").
+    //         sourcegraph.internal.updateContext({
+    //             ['implementations_LANGID']: true,
+    //         })
 
-            // Create an Implementations panel and register a locations provider.
-            createImplementationPanel(context, selector, providers)
-        }
-    }
+    //         // Create an Implementations panel and register a locations provider.
+    //         createImplementationPanel(context, selector, providers)
+    //     }
+    // }
 }
